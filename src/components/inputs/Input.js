0c33f1612389b25888c67f8f0765e5d1f7202f08
Irshad Khan <khanirshad2019@gmail.com>
2022-11-19 20:48:52 +0530

    File Selection

--- src/components/inputs/Input.js
+++ src/components/inputs/Input.js
@@ -1,41 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Input.scss";
+import { forwardRef } from "react";
 
-const Input = ({
-  id,
-  name,
-  type,
-  value,
-  className,
-  labelText,
-  placeholder,
-  handleChange,
-  style,
-}) => {
-  return (
-    <>
-      <div className="form-row">
-        {labelText && (
-          <label htmlFor={name} className="form-label">
-            {labelText}
-          </label>
-        )}
-        <input
-          id={id}
-          name={name}
-          type={type}
-          value={value}
-          onChange={handleChange}
-          placeholder={placeholder}
-          className={`form-input ${className}`}
-          autoComplete="false"
-          style={style}
-        />
-      </div>
-    </>
-  );
-};
+const Input = forwardRef((props, ref) => (
+  <div className="form-row">
+    {props.labelText && (
+      <label htmlFor={props.name} className="form-label">
+        {props.labelText}
+      </label>
+    )}
+    <input
+      ref={ref} //can pass ref in component using forwaredref
+      id={props.id}
+      name={props.name}
+      type={props.type}
+      value={props.value}
+      onChange={props.handleChange}
+      placeholder={props.placeholder}
+      onClick={props.onClick}
+      onFocus={props.onFocus}
+      onBlur={props.onBlur}
+      className={`form-input ${props.className}`}
+      style={props.style}
+      autoComplete="false"
+    />
+  </div>
+));
 
 Input.propTypes = {
   id: PropTypes.string,
@@ -47,6 +38,9 @@ Input.propTypes = {
   placeholder: PropTypes.string,
   handleChange: PropTypes.func,
   style: PropTypes.any,
+  onClick: PropTypes.func,
+  onFocus: PropTypes.func,
+  onBlur: PropTypes.func,
 };
 
 export default Input;
--- src/components/posts/post-modal/modal-box-content/ModalBoxSelection.js
+++ src/components/posts/post-modal/modal-box-content/ModalBoxSelection.js
@@ -4,16 +4,29 @@ import feeling from "./../../../../assets/images/feeling.png";
 import Input from "../../../inputs/Input";
 import useDetectOutsideClick from "./../../../../hooks/useDetectOutsideClick";
 import { useRef } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Feelings from "../../../feelings/Feelings";
 
 const ModalBoxSelection = () => {
   const { feelingIsOpen } = useSelector((state) => state.modal);
+  const { post } = useSelector((state) => state.post);
   const feelingsRef = useRef(null);
+  const fileInputRef = useRef(null);
   const [togglefeelings, setToggleFeelings] = useDetectOutsideClick(
     feelingsRef,
     feelingIsOpen
   );
+
+  const dispatch = useDispatch();
+
+  const fileInputCicked = () => {
+    //triggers input as clicked
+    fileInputRef.current.click();
+  };
+
+  const handleFileChange = (event) => {
+    console.log(event.target.files[0]);
+  };
 
   return (
     <>
@@ -24,8 +37,24 @@ const ModalBoxSelection = () => {
       )}
       <div className="modal-box-selection" data-testid="modal-box-selection">
         <ul className="post-form-list" data-testid="list-item">
-          <li className="post-form-list-item image-select">
-            <Input name="image" type="file" className="file-input" />
+          <li
+            className="post-form-list-item image-select"
+            onClick={fileInputCicked}
+          >
+            <Input
+              name="image"
+              ref={fileInputRef}
+              type="file"
+              className="file-input"
+              onClick={() => {
+                // clears previously selected image to avoid bug when selecting img 2nd time
+                if (fileInputRef.current) {
+                  console.log("clearing previous file");
+                  fileInputRef.current.value = null;
+                }
+              }}
+              handleChange={handleFileChange}
+            />
             <img src={photo} alt="" /> Photo
           </li>
           <li className="post-form-list-item">
