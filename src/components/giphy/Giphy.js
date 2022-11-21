10ec81812e9f481482a80448959c5ff6b7f15bc7
Irshad Khan <khanirshad2019@gmail.com>
2022-11-21 16:01:38 +0530

    Giphy api services and component

---
+++ src/components/giphy/Giphy.js
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleGifModal } from "../../redux-toolkit/reducers/modal/modal.reducer";
+import { updatePostItem } from "../../redux-toolkit/reducers/post/post.reducer";
+import { GiphyUtils } from "../../services/utils/giphy-utils.service";
+import Input from "../inputs/Input";
+
+import "./Giphy.scss";
+
+const Giphy = () => {
+  const { gifModalIsOpen } = useSelector((state) => state.modal);
+  const [gifs, setGifs] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  const selectGif = (gif) => {
+    dispatch(updatePostItem({ gifUrl: gif, image: "" }));
+    dispatch(toggleGifModal(!gifModalIsOpen));
+  };
+
+  useEffect(() => {
+    GiphyUtils.getTrendingGifs(setGifs, setLoading);
+  }, []);
+
+  return (
+    <>
+      <div
+        className="giphy-container"
+        id="editable"
+        data-testid="giphy-container"
+      >
+        <div className="giphy-container-picker" style={{ height: "500px" }}>
+          <div className="giphy-container-picker-form">
+            <FaSearch className="search" />
+            <Input
+              id="gif"
+              name="gif"
+              type="text"
+              labelText=""
+              placeholder="Search Gif"
+              className="giphy-container-picker-form-input"
+              handleChange={(e) =>
+                GiphyUtils.searchGif(e.target.value, setGifs, setLoading)
+              }
+            />
+          </div>
+
+          <ul
+            className="giphy-container-picker-list"
+            data-testid="unorderedList"
+          >
+            {gifs.map((gif, index) => (
+              <li
+                className="giphy-container-picker-list-item"
+                data-testid="list-item"
+                key={index}
+                onClick={() => selectGif(gif.images.original.url)}
+              >
+                <img
+                  style={{ width: "470px" }}
+                  src={`${gif.images.original.url}`}
+                  alt=""
+                />
+              </li>
+            ))}
+          </ul>
+
+          {!gifs && !loading && (
+            <ul className="giphy-container-picker-list">
+              <li className="giphy-container-picker-list-no-item">
+                No GIF found
+              </li>
+            </ul>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+export default Giphy;
