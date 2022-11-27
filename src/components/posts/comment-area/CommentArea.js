c0b7b69ea74c7024ac13f476308a146b230dbf59
Irshad Khan <khanirshad2019@gmail.com>
2022-11-27 10:08:10 +0530

    add post reactions

--- src/components/posts/comment-area/CommentArea.js
+++ src/components/posts/comment-area/CommentArea.js
@@ -4,14 +4,19 @@ import "./CommentArea.scss";
 import like from "./../../../assets/reactions/like.png";
 import Reactions from "../reactions/Reactions";
 import { useCallback, useEffect, useState } from "react";
-import { find } from "lodash";
+import { cloneDeep, filter, find } from "lodash";
 import { Utils } from "../../../services/utils/utils.service";
 import { reactionsMap } from "../../../services/utils/static.data";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { postService } from "../../../services/api/post/post.service";
+import { addReactions } from "../../../redux-toolkit/reducers/post/user-post-reaction.reducer";
+import { socketService } from "./../../../services/sockets/socket.service";
 
 const CommentArea = ({ post }) => {
-  const { reactions } = useSelector((state) => state.userPostReactions);
+  const { profile } = useSelector((state) => state.user);
+  let { reactions } = useSelector((state) => state.userPostReactions);
   const [userSelectedReaction, setUserSelectedReaction] = useState("");
+  const dispatch = useDispatch();
 
   // const reactions = [];
   const selectedUserReaction = useCallback(
@@ -31,6 +36,130 @@ const CommentArea = ({ post }) => {
 
   const addReactionPost = async (reaction) => {
     console.log("reaction", reaction, "is clicked");
+    try {
+      const reactionResponse =
+        await postService.getSinglePostReactionByUsername(
+          post._id,
+          profile.username
+        );
+      //add reaction to post
+      post = updatePostReaction(
+        reaction,
+        Object.keys(reactionResponse.data.reactions).length,
+        reactionResponse.data.reactions?.type
+      );
+
+      //add reaction to redux store
+      const postReactions = addNewReaction(
+        reaction,
+        Object.keys(reactionResponse.data.reactions).length,
+        reactionResponse.data.reactions?.type
+      );
+      reactions = [...postReactions];
+      dispatch(addReactions(reactions));
+
+      sendSocketIOReactions(
+        post,
+        reaction,
+        Object.keys(reactionResponse.data.reactions).length,
+        reactionResponse.data.reactions?.type
+      );
+
+      const reactionsData = {
+        userTo: post?.userId,
+        postId: post?._id,
+        type: reaction,
+        postReactions: post.reactions,
+        profilePicture: profile?.profilePicture,
+        previousReaction: Object.keys(reactionResponse.data.reactions).length
+          ? reactionResponse.data.reactions?.type
+          : "",
+      };
+
+      if (!Object.keys(reactionResponse.data.reactions).length) {
+        await postService.addReaction(reactionsData);
+      } else {
+        //if prev reaction exists
+        reactionsData.previousReaction = reactionResponse.data.reactions?.type;
+        if (reaction == reactionsData.previousReaction) {
+          await postService.removeReaction(
+            post?._id,
+            reactionsData.previousReaction,
+            post.reactions
+          );
+        } else {
+          await postService.addReaction(reactionsData);
+        }
+      }
+    } catch (error) {
+      // Utils.dispatchNotification(
+      //   error?.response?.data?.message,
+      //   "error",
+      //   dispatch
+      // );
+      console.log(error);
+    }
+  };
+
+  const updatePostReaction = (newReaction, hasResponse, previousReaction) => {
+    post = cloneDeep(post);
+    //if user is adding post for 1st time
+    if (!hasResponse) {
+      post.reactions[newReaction] += 1;
+    } else {
+      //if he has already given a reaction for this post
+      if (post.reactions[previousReaction] > 0) {
+        post.reactions[previousReaction] -= 1;
+      }
+      //if he select same reaction as previous then remove current
+      if (previousReaction != newReaction) {
+        post.reactions[newReaction] += 1;
+      }
+    }
+    return post;
+  };
+
+  //for redux store userPostReactions
+  const addNewReaction = (newReaction, hasResponse, previousReaction) => {
+    //remove reaction from store
+    const postReactions = filter(
+      reactions,
+      (reaction) => reaction?.postId !== post?._id
+    );
+
+    const newPostReaction = {
+      postId: post?._id,
+      type: newReaction,
+      username: profile?.username,
+      avatarColor: profile?.avatarColor,
+      profilePicture: profile?.profilePicture,
+      createdAt: `${new Date()}`,
+    };
+
+    if ((hasResponse && previousReaction !== newReaction) || !hasResponse) {
+      postReactions.push(newPostReaction);
+    }
+    return postReactions;
+  };
+
+  //emit event to server
+  const sendSocketIOReactions = (
+    post,
+    reaction,
+    hasResponse,
+    previousReaction
+  ) => {
+    const socketReactionData = {
+      userTo: post.userId,
+      postId: post._id,
+      username: profile?.username,
+      avatarColor: profile?.avatarColor,
+      profilePicture: profile?.profilePicture,
+      type: reaction,
+      postReactions: post.reactions,
+      previousReaction: hasResponse ? previousReaction : "",
+    };
+    socketService?.socket?.emit("reaction", socketReactionData);
   };
   useEffect(() => {
     selectedUserReaction(reactions);
@@ -40,7 +169,7 @@ const CommentArea = ({ post }) => {
     <>
       <div className="comment-area" data-testid="comment-area">
         <div className="like-icon reactions">
-          <div className="likes-block">
+          <div className="likes-block" onClick={() => addReactionPost("like")}>
             <div
               className={`likes-block-icons reaction-icon ${userSelectedReaction.toLowerCase()}`}
             >
--- src/services/api/post/post.service.js
+++ src/services/api/post/post.service.js
@@ -20,5 +20,26 @@ class PostService {
     const response = await axios.get(`/post/reactions/username/${username}`);
     return response;
   }
+
+  async getSinglePostReactionByUsername(postId, username) {
+    const response = await axios.get(
+      `/post/single/reaction/username/${username}/${postId}`
+    );
+    return response;
+  }
+
+  async addReaction(reaction) {
+    const response = await axios.post(`/post/reaction/`, reaction);
+    return response;
+  }
+
+  async removeReaction(postId, previousReaction, postReactions) {
+    const response = await axios.delete(
+      `/post/reaction/${postId}/${previousReaction}/${JSON.stringify(
+        postReactions
+      )}`
+    );
+    return response;
+  }
 }
 export const postService = new PostService();
