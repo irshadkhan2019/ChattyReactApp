055d113a8e4299b338e574df5f8e5283100b2dc4
Irshad Khan <khanirshad2019@gmail.com>
2022-12-14 09:03:44 +0530

    Emoji Picker in MessageInput

--- src/components/chat/window/message-input/MessageInput.js
+++ src/components/chat/window/message-input/MessageInput.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Input from "../../../inputs/Input";
 import Button from "../../../button/Button";
@@ -6,15 +6,35 @@ import { FaPaperPlane } from "react-icons/fa";
 import gif from "./../../../../assets/images/gif.png";
 import photo from "./../../../../assets/images/photo.png";
 import feeling from "./../../../../assets/images/feeling.png";
+import loadable from "@loadable/component";
 import "./MessageInput.scss";
 
+const EmojiPickerComponent = loadable(() => import("./EmojiPicker"), {
+  fallback: <p id="loading">Loading....</p>,
+});
+
 const MessageInput = ({ setChatMessage }) => {
+  const [showEmojiContainer, setShowEmojiContainer] = useState();
+
   return (
     <>
+      {showEmojiContainer && (
+        <EmojiPickerComponent
+          onEmojiClick={(event, eventObject) => {
+            console.log("Event object", eventObject, event);
+          }}
+          pickerStyle={{ width: "352px", height: "447px" }}
+        />
+      )}
       <div className="chat-inputarea" data-testid="chat-inputarea">
         <form>
           <ul className="chat-list" style={{ borderColor: "#50b5ff" }}>
-            <li className="chat-list-item">
+            <li
+              className="chat-list-item"
+              onClick={() => {
+                setShowEmojiContainer(false);
+              }}
+            >
               <Input
                 id="image"
                 name="image"
@@ -25,10 +45,20 @@ const MessageInput = ({ setChatMessage }) => {
 
               <img src={photo} alt="" />
             </li>
-            <li className="chat-list-item">
+            <li
+              className="chat-list-item"
+              onClick={() => {
+                setShowEmojiContainer(false);
+              }}
+            >
               <img src={gif} alt="" />
             </li>
-            <li className="chat-list-item">
+            <li
+              className="chat-list-item"
+              onClick={() => {
+                setShowEmojiContainer(!showEmojiContainer);
+              }}
+            >
               <img src={feeling} alt="" />
             </li>
           </ul>
