6d76241a7ae211e0b1f551ee97baa9309d6525c3
Irshad Khan <khanirshad2019@gmail.com>
2023-05-28 10:18:08 +0530

    Create Peer Connection objects

--- src/services/sockets/socket.service.js
+++ src/services/sockets/socket.service.js
@@ -1,14 +1,18 @@
 import { io } from "socket.io-client";
 import { newRoomCreated, updateActiveRooms } from "./room.service";
+import { store } from "../../redux-toolkit/store";
+import Peer from "simple-peer"
 
 class SocketService {
   socket;
+  peers={};
   setupSocketConnection() {
     // connect to node js app
     this.socket = io(process.env.REACT_APP_BASE_ENDPOINT, {
       transports: ["websocket"],
       secure: true,
     });
+    this.peers={}
     this.socketConnectionEvents();
   }
   socketConnectionEvents() {
@@ -33,10 +37,21 @@ class SocketService {
     this.socket.on("active-rooms", (activeRooms) => {
       updateActiveRooms(activeRooms);
     });
+    //new users recieves that others have prepared to accept connections
+    this.socket.on("conn-init", (data) => {
+      const {conUserSocketId}=data;
+      
+      this.prepareNewPeerConnection(conUserSocketId,true)
+    });
 
     // new users joins and sends his socket id to prepare for connection
-    this.socket.on("conn-prepare", (socketId) => {
-        console.log("PREpare conn",socketId)
+    this.socket.on("conn-prepare", (data) => {
+        const {conUserSocketId}=data;
+        this.prepareNewPeerConnection(conUserSocketId,false)
+        //tells the new joined user that i am ready for connection
+        this.socket.emit('conn-init',{
+          conUserSocketId
+        })
       });
   }
   // create a room and emit event to server
@@ -49,6 +64,25 @@ class SocketService {
     this.socket.emit("get-active-room");
   };
 
+  getConfiguration=()=>{
+    const turnIceServers=null;
+
+    if(turnIceServers){
+      //use turn server creds
+    }else{
+        console.warn("using ony stun server");
+        // to get internet details connection
+        return{
+          iceServer:[
+            {
+              urls:'stun:stun.l.google.com:19302'
+            }
+          ]
+        }
+    }
+
+  }
+
   // To Join a room,emit event to server
   joinRoom = (user, roomId) => {
     this.socket.emit("room-join", { user, roomId });
@@ -58,5 +92,42 @@ class SocketService {
     console.log("leaving room", user.profile._id, roomId);
     this.socket.emit("room-leave", { user, roomId });
   };
+
+
+  //takes new joinee socket id and if the current user is initiator or not
+  prepareNewPeerConnection=(conUserSocketId,isInitiator)=>{
+    const localStream=store.getState().room.localStream;
+
+    if(isInitiator){
+      console.log("preparing new peer connection as initiator")
+    }else{
+      console.log("preparing new peer connection not as  initiator")
+    }
+    // create peer obj for user who has just joined room 
+    this.peers[conUserSocketId]=new Peer({
+      initiator:isInitiator,
+      config:this.getConfiguration(),//prepares our internet info
+      stream:localStream //attach our localStream ,to broadcast to other users
+    });
+  
+    //after getting our sdp data and ice candidate via getConfiguration(),signal is triggerd,
+    //we need to share this with other users
+    this.peers[conUserSocketId].on('signal',data=>{
+      const signalData={
+        signal:data,//data received via getConfiguration()
+        conUserSocketId,//to which  user we want to pass this signaling data
+      }
+     // console.log("signalData",signalData)
+      // todo :pass signaling data to other user
+    })
+    //event triggers when connection is established with other user
+    this.peers[conUserSocketId].on("stream",(remoteStream)=>{
+      //Todod add new remote stream to server store
+      console.log("remoteStream",remoteStream)
+    })
+
+    console.log("PEERS:::",this.peers)
+
+  }
 }
 export const socketService = new SocketService();
